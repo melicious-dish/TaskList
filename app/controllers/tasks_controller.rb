20729dcf23824a737f6c9168b8014f820db3a6ae
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-09-18 15:27:28 -0700

    add tasks via terminal update the html and erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,24 @@
-TASKS = [ { task: "Pluck Eyebrows", status: "incomplete"},
- { task: "Shave toes", status: "incomplete"},
- { task: "Collect dust bunnies", status: "complete"}]
+# TASKS = [ { task: "Pluck Eyebrows", status: "incomplete"},
+#  { task: "Shave toe", status: "incomplete"},
+#  { task: "Collect dust bunnies", status: "complete"}]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+# params - acts like a hash, always come in as a string
+# id will come from url /books/2
+
+
+#2 use the param in the controller
+  # key in params NEED TO MATCH the key in routes
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: [task_id])
+    if @task.nil?
+      head :not_found  #or 404.. http head: head is general to all controllers. don't send body, just send error code.
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
+
+  # 1- need to define a route that contians a param. then use the param in the controller
+  # :id = this part is a parameter
+  get '/tasks/:id', to: 'tasks#show'
 end
