cfb4382f68f98218e6188b84447461727e3be910
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-09-20 17:20:38 -0700

    add a form to add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,11 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-# params - acts like a hash, always come in as a string
-# id will come from url /books/2
+  # params - acts like a hash, always come in as a string
+  # id will come from url /books/2
 
 
-#2 use the param in the controller
+  #2 use the param in the controller
   # key in params NEED TO MATCH the key in routes
 
   def show
@@ -20,5 +20,28 @@ class TasksController < ApplicationController
     if @task.nil?
       head :not_found  #or 404.. http head: head is general to all controllers. don't send body, just send error code.
     end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(
+      action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date]
+    ) #instantiate a new book
+    is_successful_save = task.save
+
+    if is_successful_save # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the book in the list
+    else # save failed :(
+      render :new # show the new book form view again
+    end
+  end
+
+  def edit
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
 
   # 1- need to define a route that contians a param. then use the param in the controller
   # :id = this part is a parameter
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+
 end
